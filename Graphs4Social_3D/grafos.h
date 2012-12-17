c16d3e5666eb31ef58f55f0b3378100874369e73
Administrador <Administrador@Bruno_Portatil.dei.isep.ipp.pt>
2012-12-17 17:32:37 +0000

    Bruno: Obter os amigos que tenham em comum X tags (falta apenas sem?ntica). Fix nos amigos n?vel 3 para obter lista ao inv?s do n?mero

---
+++ Graphs4Social_3D/grafos.h
@@ -0,0 +1,43 @@
+#ifndef _GRAFO_INCLUDE
+#define _GRAFO_INCLUDE
+
+#define _MAX_NOS_GRAFO 100
+#define _MAX_ARCOS_GRAFO 200
+
+#define NORTE_SUL	0
+#define ESTE_OESTE	1
+#define PLANO		2
+
+#define VELOCIDADE_VERTICAL 0.02
+#define VELOCIDADE_HORIZONTAL 0.02
+#define DIMEMSAO_CAMARA 4
+
+typedef struct No{
+	float x,y,z,largura;
+}No;
+
+typedef struct Arco{
+	int noi,nof;
+	float peso,largura;
+}Arco;
+
+extern No nos[];
+extern Arco arcos[];
+extern int numNos, numArcos;
+
+void addNo(No);
+void deleteNo(int);
+void imprimeNo(No);
+void listNos();
+No criaNo(float, float, float);
+
+void addArco(Arco);
+void deleteArco(int);
+void imprimeArco(Arco);
+void listArcos();
+Arco criaArco(int, int, float, float);
+
+void gravaGrafo();
+void leGrafo();
+
+#endif

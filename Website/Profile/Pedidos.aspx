e511bc5766982017936551d04b186ca363496418
MpApQ <MpApQ@localhost>
2013-01-07 14:39:10 +0000

    Pedidos actualizados

--- Website/Profile/Pedidos.aspx
+++ Website/Profile/Pedidos.aspx
@@ -1,27 +1,61 @@
 ﻿<%@ Page Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Pedidos.aspx.cs" Inherits="Profile_Pedidos" %>
 
 <asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <script type="text/javascript">
 
+        function pageLoad(sender, args) {
+            $("#<%=btt.ClientID %>").click(function () {
 
+                $("#dialog").dialog("open");
 
-    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" OnRowCommand="GridView1_RowCommand1">
-        <Columns>
+            });
 
-            <asp:TemplateField>
-                <ItemTemplate>
-                    <asp:LinkButton ID="aceitar" runat="server" CommandName="Aceitar" Text="Aceitar"></asp:LinkButton>
-                </ItemTemplate>
-            </asp:TemplateField>
-            
-            <asp:TemplateField>
-                <ItemTemplate>
-                    <asp:LinkButton ID="rejeitar" runat="server" CommandName="Rejeitar" Text="Rejeitar"></asp:LinkButton>
-                </ItemTemplate>
-            </asp:TemplateField>
+            $("#dialog").dialog({
+                autoOpen: false,
+                buttons: {
+                    "Ok": function () {
+                        var v = $("#MainContent_btt").attr("onclick");
 
-        </Columns>
-    </asp:GridView>
+                        var f = v.split(",");
 
+                        var s = f[4].replace('"', '');
+                        var s1 = s.replace('"', '');
 
+                        var nome = s1.split("=");
+
+                        window.location = "Pedidos.aspx?user=" + nome[1] + "&forca=" + $("#dropdown option:selected").text();
+                    },
+                    "Cancelar": function () {
+                        $(this).dialog("close");
+                    }
+                }
+            });
+        }
+
+    </script>
+    <table id="pedidosTable" runat="server">
+         
+    </table>
+
+    <asp:Button ID="btt" runat="server" OnClientClick="return false;"/>
+    <asp:Button ID="btt1" runat="server" UseSubmitBehavior="false" OnClick="btt1_Click"/>
+    <asp:DropDownList ID="dropi" runat="server" AutoPostBack="True"/>
+    <asp:Image ID="img" runat="server" />
+    <asp:Label ID="Label1" runat="server" Visible="false"></asp:Label>
+    <br />
+    <br />
+    <br />
+    <asp:Label ID="Label2" runat="server" Visible="false" style="font-weight: 700"></asp:Label>
+
+    <div id="dialog" title="Força Ligação">
+        Escolha a força da ligação:<br />
+        <select id="dropdown">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+        </select>
+    </div>
 
 </asp:Content>
--- Website/Profile/Pedidos.aspx.cs
+++ Website/Profile/Pedidos.aspx.cs
@@ -4,44 +4,233 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
 using Graphs4Social_AR;
+using System.Reflection;
 
 public partial class Profile_Pedidos : System.Web.UI.Page
 {
+
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (!IsPostBack)
+        btt.Visible = false;
+        btt1.Visible = false;
+        dropi.Visible = false;
+        int forca = -1;
+        if (Request.QueryString["user"] != null && Request.QueryString["forca"] != null)
+        {
+            try
+            {
+                forca = Convert.ToInt32(Request.QueryString["forca"]);
+
+                bool a = Graphs4Social_AR.Ligacao.AceitarPedido(Profile.UserName, Request.QueryString["user"], Convert.ToInt32(Request.QueryString["forca"]));
+                if (!a)
+                {
+                    Graphs4Social_AR.Ligacao.RejeitarPedido(Profile.UserName, Request.QueryString["user"]);
+                }
+
+                Label1.Visible = true;
+
+                IList<string> profile = Graphs4Social_AR.User.LoadProfileByUser(Request.QueryString["user"]);
+
+                string imagem = "", sexo = "";
+
+                foreach (string element in profile)
+                {
+                    if (element.Contains("Imagem:"))
+                    {
+                        imagem = element;
+                    }
+                    else if (element.Contains("Sexo:"))
+                    {
+                        sexo = element;
+                    }
+
+                }
+
+                if (!imagem.Equals(""))
+                {
+                    if (imagem.Split(':')[1].Contains("Masculino") ||
+                                imagem.Split(':')[1].Contains("Feminino") ||
+                                imagem.Split(':')[1].Contains("Digo depois") ||
+                                imagem.Split(':')[1].Contains("Indeciso"))
+                    {
+                        if (!sexo.Equals(""))
+                        {
+                            FillImagemPerfil(img, sexo.Split(':')[1] + ".png");
+                        }
+                    }
+                    else
+                        FillImagemPerfil(img, imagem.Split(':')[1]);
+                }
+                else
+                {
+                    FillImagemPerfil(img, "Indeciso.png");
+                }
+
+                //Globalization aqui!!!
+                Label2.Text = "Amigo adicionado com sucesso";
+                Label2.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                Label2.Text = "Ocorreu um erro. Tente mais tarde";
+                Label2.Visible = true;
+            }
+        }
+        else
         {
 
-            BindGridView();
+            if (!IsPostBack)
+            {
 
+                IList<User> list = Graphs4Social_AR.User.LoadAllPedidosUser(Profile.UserName);
+
+                HtmlTableRow rw = new HtmlTableRow();
+                HtmlTableCell cell = new HtmlTableCell();
+
+                HtmlTable htmltb = new HtmlTable();
+                HtmlTableRow rw1 = new HtmlTableRow();
+                HtmlTableRow rw2 = new HtmlTableRow();
+                HtmlTableRow rw3 = new HtmlTableRow();
+                HtmlTableRow rw4 = new HtmlTableRow();
+
+                HtmlTableCell cell1 = new HtmlTableCell();
+                HtmlTableCell cell2 = new HtmlTableCell();
+                HtmlTableCell cell3 = new HtmlTableCell();
+                HtmlTableCell cell4 = new HtmlTableCell();
+
+
+
+                //mudar length para list.Count
+                for (int i = 0; i < list.Count; i++)
+                {
+
+
+                    Image img1 = new Image();
+                    img1.ImageUrl = "~/Images/cookie-avatar-350x350.jpeg";
+                    img1.Height = 150;
+                    img1.Width = 150;
+                    img1.BorderWidth = 2;
+                    img1.BorderColor = System.Drawing.Color.Gray;
+                    cell1.Controls.Add(img1);
+
+
+                    Label name = new Label();
+                    name.Text = list[i].Username;
+                    //name.Text = "texto something";
+                    cell2.Controls.Add(name);
+
+
+                    Button btA = new Button();
+                    btA = (Button)CloneObject(btt);
+                    btA.Text = "Aceitar";
+                    btA.PostBackUrl = "~/Profile/Pedidos.aspx?user=" + list[i].Username;
+                    btA.Visible = true;
+                    cell3.Controls.Add(btA);
+
+
+
+                    Button btR = new Button();
+                    btR = (Button)CloneObject(btt1);
+                    btR.Text = "Rejeitar";
+                    btR.PostBackUrl = "~/Profile/Pedidos.aspx?user=" + list[i].Username;
+                    btR.Visible = true;
+                    cell4.Controls.Add(btR);
+
+
+                    rw1.Controls.Add(cell1);
+                    htmltb.Controls.Add(rw1);
+
+
+                    rw2.Controls.Add(cell2);
+                    htmltb.Controls.Add(rw2);
+
+
+                    rw3.Controls.Add(cell3);
+                    htmltb.Controls.Add(rw3);
+
+
+                    rw4.Controls.Add(cell4);
+                    htmltb.Controls.Add(rw4);
+
+
+                    cell.Controls.Add(htmltb);
+
+
+                    if (i % 3 == 0 && i != 0)
+                    {
+                        pedidosTable.Controls.Add(rw);
+
+                        rw = new HtmlTableRow();
+
+                        rw.Cells.Add(cell);
+
+                    }
+                    else
+                    {
+                        rw.Cells.Add(cell);
+                    }
+
+                    cell = new HtmlTableCell();
+
+                    cell1 = new HtmlTableCell();
+                    cell2 = new HtmlTableCell();
+                    cell3 = new HtmlTableCell();
+                    cell4 = new HtmlTableCell();
+
+
+                    rw1 = new HtmlTableRow();
+                    rw2 = new HtmlTableRow();
+                    rw3 = new HtmlTableRow();
+                    rw4 = new HtmlTableRow();
+
+                    htmltb = new HtmlTable();
+                }
+
+                if (list.Count % 3 > 0)
+                    pedidosTable.Controls.Add(rw);
+
+                btt.Visible = false;
+                btt1.Visible = false;
+                dropi.Visible = false;
+            }
+            else
+            {
+                btt.Visible = false;
+                btt1.Visible = false;
+                dropi.Visible = false;
+
+            }
         }
     }
 
-    public void BindGridView()
+    private void FillImagemPerfil(Image img, string image)
     {
-        GridView1.DataSource = Graphs4Social_AR.User.LoadAllPedidosUser(Profile.UserName);
-        GridView1.DataBind();
+        img.ImageUrl = "~/Images/" + image;
+        img.Height = 150;
+        img.Width = 150;
+        img.BorderWidth = 2;
+        img.BorderColor = System.Drawing.Color.Gray;
+        Label1.Text = Request.QueryString["user"];
     }
 
-
-    protected void GridView1_RowCommand1(object sender, GridViewCommandEventArgs e)
+    public static Object CloneObject(object o)
     {
-        if (e.CommandName == "Rejeitar")
+        Type t = o.GetType();
+        PropertyInfo[] info = t.GetProperties();
+        Object p = t.InvokeMember("", System.Reflection.BindingFlags.CreateInstance, null, o, null);
+        foreach (PropertyInfo pi in info)
         {
-            GridViewRow gvr = (GridViewRow)(((LinkButton)e.CommandSource).NamingContainer);
-            int rowIndex = gvr.RowIndex;
-
-            
-
+            if (pi.CanWrite)
+            {
+                pi.SetValue(p, pi.GetValue(o, null), null);
+            }
         }
-        else if (e.CommandName == "Aceitar")
-        {
-            GridViewRow gvr = (GridViewRow)(((LinkButton)e.CommandSource).NamingContainer);
-            int rowIndex = gvr.RowIndex;
+        return p;
+    }
+    protected void btt1_Click(object sender, EventArgs e)
+    {
 
-
-
-        }
     }
 }

d9bd8aca687d64130eabd5784b9e64ef8612bfd2
Administrador <Administrador@Bruno_Portatil.dei.isep.ipp.pt>
2013-01-09 09:16:43 +0000

    Bruno: Fix grau medio de separacao (para prevenir erros da Database Prolog)

---
+++ Graphs4Social_WebService/App_Code/IGraphs4Social_Service.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+[ServiceContract]
+public interface IGraphs4Social_Service
+{
+	[OperationContract]
+	void DoWork();
+}
---
+++ IA/WebConsumer/WebConsumer/WebConsumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebConsumer
+{
+    class Program
+    {
+      static  void Main(string[] args)
+      {
+          var proxy =
+               new moduloIA.ModuloIaClient();
+          Console.WriteLine("Nome do user:");
+          string x = Console.ReadLine();
+          int tamanhoRede = proxy.tamanhoRede(2, x);
+          Console.Write(tamanhoRede);
+      }
+    }
+}
+

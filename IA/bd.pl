119938d96957ae907c392654a352d75e7430c78c
Administrador <Administrador@Bruno_Portatil.dei.isep.ipp.pt>
2013-01-09 14:13:08 +0000

    Bruno: Cut no Grau medio separa??o.
    Pag. Administra??o site

---
+++ IA/bd.pl
@@ -0,0 +1,221 @@
+user(pedro).
+user(bruno).
+user(catia).
+user(hugo).
+user(mario).
+user(tiago).
+user(carlos).
+user(sara).
+
+%lig(user1,user2,forca1para2,forca2para1,[tags1para2],[tags2para1])
+
+tag(porto,[bruno,pedro,catia,hugo,carlos,sara]).
+tag(chelsea,[bruno,sara,catia]).
+tag(comida,[mario,bruno,hugo,sara]).
+
+traduzir(blues,chelsea).
+
+lig(pedro,bruno,5,[amigo]).
+lig(bruno,pedro,1,[ursologia]).
+lig(bruno,catia,2,[ese]).
+lig(catia,bruno,5,[isep]).
+lig(tiago,catia,2,[ese]).
+lig(catia,tiago,5,[isep]).
+lig(hugo,catia,5,[ese]).
+lig(catia,hugo,5,[isep]).
+lig(bruno,mario,2,[contador]).
+lig(mario,bruno,5,[cavendish]).
+lig(hugo,mario,3,[myva]).
+lig(mario,hugo,5,[myva]).
+lig(carlos,mario,2,[testeMaven]).
+lig(mario,carlos,1,[testeMaven]).
+lig(sara,catia,3,[testeSug]).
+lig(catia,sara,2,[testeSug]).
+
+%Verificar 
+minimoMaven(3).
+percentagemMaven(1.25).
+
+
+%ligado(A,B):-lig(A,B,_,_), write('ligado').
+ligado(A,B):-lig(A,B,_,_).
+
+%ligacoes(User,Resposta) nivel 2
+ligacoes(U,L):-findall(X,ligado(U,X),L).
+
+%tamanho(User,Resposta) nivel 2
+tamanho2(U,R):-redeNivel2(U,L),length(L,R).
+
+tamanho3(U,R):-redeNivel3(U,L),cl(L,R).
+
+redeNivel2(U,R):-user(U),findall(X,ligado(U,X),R).
+
+redeNivel3(U,R):-user(U),ligacoes(U,L),percorreUser(U,L,R).
+%percorre(user original, lista ligacoes, resposta)
+percorreUser(U,L,R):-append([U],L,V),percorre(L,V,R).
+
+%percorre(Amigo a verificar|restantes,Já verificados,resposta
+percorre([A|L],V,R):-somaRede(A,V,RV,RX),percorre(L,RV,RS),append(RX,RS,R).
+percorre([],_,[]).
+
+somaRede(A,V,RV,[A|RX]):-ligacoes(A,TL),somaL(TL,V,RV,RX).
+
+somaL([],V,V,[]).
+somaL([LA|LB],V,RV,RX):-member(LA,V),!,somaL(LB,V,RV,RX).
+somaL([LA|LB],V,RV,[LA|RS]):-append([LA],V,XV),somaL(LB,XV,RV,RS).
+
+
+%obter amigos por tags
+%User, Lista Tags, Amigos
+amigosTag(U,T,R):-ligacoes(U,L),traduz(T,LT),filtraAmigos(L,LT,R).
+
+traduz([T|TR],[F|FR]):-traduzir(T,F),!,tag(F,_),traduz(TR,FR).
+traduz([T|TR],[T|R]):-tag(T,_),traduz(TR,R).
+traduz([],[]).
+
+%ligacoes,tags
+filtraAmigos([LA|LB],T,[LA|RX]):-verificaTags(LA,T),!,filtraAmigos(LB,T,RX).
+filtraAmigos([_|LB],T,R):-filtraAmigos(LB,T,R).
+filtraAmigos([],_,[]).
+
+verificaTags(U,[TA|TB]):-tag(TA,L),member(U,L),verificaTags(U,TB).
+verificaTags(_,[]).
+
+%sugereAmigos
+sugereAmigos(U,R):-redeNivel3(U,L),filtraNaoLigacoes(U,L,F),deletelist(L,F,LF),criaSugestao(U,LF,R),!.
+
+filtraNaoLigacoes(U,[LA|LB],[LA|R]):-ligado(U,LA),!,filtraNaoLigacoes(U,LB,R).
+filtraNaoLigacoes(U,[_|LB],R):-filtraNaoLigacoes(U,LB,R).
+filtraNaoLigacoes(_,[],[]).
+
+
+criaSugestao(U,[FA|FB],[FA|R]):-findall(X,tag(X,_),LT),semelhante(U,FA,LT),!,criaSugestao(U,FB,R).
+criaSugestao(U,[_|FB],R):-criaSugestao(U,FB,R).
+criaSugestao(_,[],[]).
+
+%LT = lista Tags
+semelhante(U,F,[TA|LT]):-tag(TA,L),member(U,L),member(F,L),!,semelhante(U,F,LT).
+semelhante(U,F,[TA|_]):-tag(TA,L),(member(U,L);member(F,L)),!,fail.
+semelhante(U,F,[_|LT]):-semelhante(U,F,LT).
+semelhante(_,_,[]).
+
+
+%Ver Calculo Equilibrio vertices Estrela
+%Maven(T,R):-tag(T,U),cl(U,TM),minimoMaven(X),TM>X,calculaMaven(T,U,TM,R).
+
+%calculaMaven(T,[U|UR],TV,UA,VA,R):-forca(T,U,TV,CL),CL>VA,!,calculaMaven(T,UR,TV,U,CL,R).
+%calculaMaven(T,[_|UR],TV,UA,VA,R):-calculaMaven(T,UR,RV,UA,VA,R).
+%calculaMaven(_,[],_,UA,_,UA).
+
+maven(T,R):-tag(T,U),cl(U,TM),minimoMaven(X),TM>X,calculaMaven(T,U,F,TM),!,estrela(U,F,R).
+%Tag, users, forcas, total vertices
+calculaMaven(T,[U|UR],[F|FR],TV):-forca(T,U,TV,F),calculaMaven(T,UR,FR,TV).
+calculaMaven(_,[],[],_).
+
+forca(T,U,TV,R):-tag(T,L),deletelist(L,[U],LF),calculaForcaVertices(U,LF,RS),R is RS/TV. %??.
+%user, vertices c/ tag
+calculaForcaVertices(U,[A|L],R):-ligado(U,A),!,calculaForcaVertices(U,L,RS),R is RS + 1.
+calculaForcaVertices(U,[_|L],R):-calculaForcaVertices(U,L,R).
+calculaForcaVertices(_,[],0).
+
+%estrela(U,F,R):-estrela(U,F,F,R).
+
+%estrela([U|UR],[F|FR],OF,U):-percorreForca(F,OF),!.
+%estrela([_|UR],[_|FR],OF,R):-estrela(UR,FR,OF,R).
+%estrela([],[],_,_):-write('Nao existe ninguem com forca maven'),fail.
+
+estrela(U,F,R):-estrela(U,F,U,F,R).
+%manipulados e originais, max actuais, resposta
+estrela([U|UR],[F|FR],OU,OF,U):-percorreForca(U,F,OU,OF),!.
+estrela([_|UR],[_|FR],OU,OF,R):-estrela(UR,FR,OU,OF,R).
+estrela([],[],_,_,_):-fail.
+
+%forca
+percorreForca(U,F,[U|UR],[F|FR]):-percorreForca(U,F,UR,FR).
+percorreForca(U,F,[_|UR],[V|FR]):-percentagemMaven(X),T is V*X,F>T,percorreForca(U,F,UR,FR).
+percorreForca(_,_,[],[]).
+
+grafoComum(U,U,_):-fail.
+grafoComum(UA,UB,G):-redeNivel2(UA,R1),redeNivel2(UB,R2),intersect(R1,R2,G).
+
+
+intersect([ ],_,[ ]).
+intersect([X|L1],L2,[X|LI]):-member(X,L2),!,intersect(L1,L2,LI).
+intersect([_|L1],L2, LI):- intersect(L1,L2,LI).
+
+cl(L, R) :-
+        call(L, 0 , R).
+
+% 1 - Terminating condition
+call([], Count, Count).
+
+% 2 - Recursive rule
+call([_|T], Count, R) :-
+        Count1 is Count + 1,
+        call(T, Count1, R).
+
+
+add(X,[],[X]).
+add(X,[A|L],[A|L1]):- add(X,L,L1).
+
+notmember(X,L):-(member(X,L), !, fail);true.
+
+
+deletelist([], _, []).                  
+deletelist([X|Xs], Y, Z) :- member(X, Y), deletelist(Xs, Y, Z), !.
+deletelist([X|Xs], Y, [X|Zs]) :- deletelist(Xs, Y, Zs).
+
+
+%Branch and bound
+camForte(O,D,Perc):-findall(P,camPesado(O,D,P),L),reverse(L,[Perc|_]).
+
+
+camPesado(O,D,Perc):- go1([(0,[O])],D,P),reverse(P,Perc). 
+go1([(_,Pr)|_],D,Pr):- Pr=[D|_]. 
+go1([(_,[D|_])|R],D,Perc):- !, go1(R,D,Perc).
+go1([(C,[Ult|T])|O],D,Perc):- findall((NC,[Z,Ult|T]),	(proximo_no(Ult,T,Z,C1),NC is C+C1),L),
+ 	append(O,L,NPerc),sort(NPerc,NPerc1),go1(NPerc1,D,Perc). 
+
+proximo_no(X,T,Z,C):- lig(X,Z,C,_), not member(Z,T). 
+
+
+
+%primeiro em largura
+camCurto(Orig,Dest,Perc) :- largura([[Orig]],Dest,P), reverse(P,Perc). 
+
+largura([Prim|_],Dest,Prim) :- Prim=[Dest|_]. 
+largura([[Dest|_]|Resto],Dest,Perc) :- !, largura(Resto,Dest,Perc). 
+largura([[Ult|T]|Outros],Dest,Perc):-findall([Z,Ult|T],proximo_no(Ult,T,Z),Lista),
+ append(Outros,Lista,NPerc), largura(NPerc,Dest,Perc). 
+
+proximo_no(X,T,Z) :- ligado(X,Z), not member(Z,T). 
+
+
+%grauMedio separacao (s/ cut/complexidade)
+grauMedio(R):-limpaDb,findall(X,user(X),LU),assert(lUsers(LU)),grauMedio(LU,V,C),limpaDb,R is V/C.
+
+limpaDb:-retractall(verificado(_,_)),retractall(lUsers(_)).
+
+%passos, contagem caminhos		
+grauMedio([U|UR],V,C):-somaCaminhos(U,P,NC),grauMedio(UR,VA,RA),V is VA + P, C is RA + NC.
+grauMedio([],0,0).
+
+%passos, n caminhos
+somaCaminhos(U,P,NC):-lUsers(LU),contaPassos(U,LU,P,NC).
+
+contaPassos(U,[U|UP],P,NC):-!,contaPassos(U,UP,P,NC).
+contaPassos(U,[UA|UP],P,NC):-verificado(UA,U),!,contaPassos(U,UP,P,NC).
+
+contaPassos(U,[UA|UP],P,NC):-camCurto(U,UA,C),length(C,PX),assert(verificado(U,UA)),
+							contaPassos(U,UP,PR,NR),NC is NR+1, P is PX + PR.
+contaPassos(_,[],0,0).
+
+%contaPassos([C|CR],P):-contaPassos(CR,PR), length(C,X), P is PR + X.
+%contaPassos([],0).
+
+runTamanhoNivel2(U):-tamanho2(U,X),write(X).
+runTamanhoNivel3(U):-tamanho3(U,X),write(X).
+	
+run2 :-
+	write('insira o nome do user a verificar: (termine com ponto .)'),nl,read(X),
+	ligado(X,_),!,nl,write('Existe').
---
+++ Website/Profile/Inicio.aspx
@@ -0,0 +1,188 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Inicio.aspx.cs" Inherits="Profile_Inicio" MasterPageFile="~/Site.master" %>
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+
+    <table>
+        <tr>
+            <td>
+                <asp:Image ID="Image1" runat="server" />
+            </td>
+            <td style="text-align:right; width:250px; font-size:xx-large;font-variant:small-caps;">
+                <hgroup id="tituloPerfil" class="title" runat="server">
+                </hgroup>
+            </td>
+        </tr>
+    </table>
+    <br />
+    <asp:Button ID="Button1" runat="server" Text="" Visible="false" OnClick="Button1_Click" CssClass="btProfile"/>
+    
+    <asp:DropDownList ID="DropDownList1" runat="server" CssClass="listProfile" AutoPostBack="false" Visible="false">
+        <asp:ListItem Value="1">1</asp:ListItem>
+        <asp:ListItem Value="2">2</asp:ListItem>
+        <asp:ListItem Value="3">3</asp:ListItem>
+        <asp:ListItem Value="4">4</asp:ListItem>
+        <asp:ListItem Value="5">5</asp:ListItem>
+    </asp:DropDownList>
+
+    <asp:DropDownList ID="tagsRelacao" runat="server" CssClass="tagsProfile" AutoPostBack="false" Visible="false">
+        
+    </asp:DropDownList>
+
+    <br />
+
+    <asp:Label ID="Label9" runat="server" Text="" Visible="false"></asp:Label>
+    <asp:Label ID="Label1" runat="server" Text="" Visible="false"></asp:Label>
+
+    <br />
+
+    <asp:Label ID="Label10" runat="server" Text="" Visible="false"></asp:Label>
+    <asp:Label ID="Label2" runat="server" Text="" Visible="false"></asp:Label>
+    
+    <br />
+
+    <asp:Label ID="Label11" runat="server" Text="" Visible="false"></asp:Label>
+    <asp:Label ID="Label3" runat="server" Text="" Visible="false"></asp:Label>
+
+    <br /><br />
+
+    <asp:Label ID="Label12" runat="server" Text="" Visible="false"></asp:Label>
+    <asp:Label ID="Label4" runat="server" Text="" Visible="false"></asp:Label>
+
+    <br />
+
+    <asp:Label ID="Label13" runat="server" Text="" Visible="false"></asp:Label>
+    <asp:Label ID="Label5" runat="server" Text="" Visible="false"></asp:Label>
+
+    <br />
+
+    <asp:Label ID="Label14" runat="server" Text="" Visible="false"></asp:Label>
+    <asp:Label ID="Label6" runat="server" Text="" Visible="false"></asp:Label>
+
+    <br /><br />
+
+    <asp:Label ID="Label15" runat="server" Text="" Visible="false"></asp:Label>
+    <asp:HyperLink ID="HyperLink1" runat="server">
+        <asp:Label ID="Label7" runat="server" Text="" Visible="false"></asp:Label>
+    </asp:HyperLink>
+
+    <br /><br />
+
+    <asp:Label ID="Label16" runat="server" Text="" Visible="false"></asp:Label>
+    <asp:HyperLink ID="HyperLink2" runat="server">
+        <asp:Label ID="Label8" runat="server" Text="" Visible="false"></asp:Label>
+    </asp:HyperLink>
+
+    <br />
+    
+    <div id="profiletab">
+        <ul>
+            <li><a href="#tabs-1"><asp:Label ID="Label19" runat="server" Text=""></asp:Label></a></li>
+            <li><a href="#tabs-2"><asp:Label ID="Label20" runat="server" Text=""></asp:Label></a></li>
+        </ul>
+        <div id="tabs-1">
+            <asp:Label ID="Label18" runat="server" Text=""></asp:Label>
+
+            <br />
+            <br />
+            <table id="tabelaDeAmigos" runat="server"></table>
+
+            <table>
+                <tr>
+                    <td>
+                        <asp:Image ID="Image2" runat="server" Visible="false" ImageAlign="Middle" />
+                    </td>
+                    <td>
+                        <asp:Image ID="Image3" runat="server" Visible="false" ImageAlign="Middle" /></td>
+                </tr>
+            </table>
+        </div>
+        <div id="tabs-2">
+            <asp:Label ID="Label17" runat="server" Text="Label"></asp:Label>
+
+            
+
+
+        </div>
+    </div>
+
+                <div id="tagcloud">
+                    <a href="#" rel="0.1">Lorem</a>
+                    <a href="#" rel="2">ipsum</a>
+                    <a href="#" rel="9">dolor</a>
+                </div>
+
+    
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css" />
+    <script src="http://code.jquery.com/jquery-1.8.3.js"></script>
+    <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
+	<script src="Scripts/jquery.tagcloud.js" type="text/javascript" charset="utf-8"></script>
+
+    <script>
+        $(function () {
+            $("#tagcloud a").tagcloud({
+                size: {
+                    start: 14,
+                    end: 18,
+                    unit: 'px'
+                },
+                color: {
+                    start: '#cde',
+                    end: '#f52'
+                }
+            })
+        });</script>
+
+    <style>
+        #tagcloud a {
+            text-decoration: none;
+        }
+
+            #tagcloud a:hover {
+                text-decoration: underline;
+            }
+
+        #tagcloud {
+            margin: 25px auto;
+            font: 75% Arial, "MS Trebuchet", sans-serif;
+        }
+
+        .btProfile {
+            width: 200px;
+            height: 40px;
+            padding: 5px;
+            color: #333333;
+            background-color: white;
+            border-color: gray;
+            border-style: solid;
+            font-variant: small-caps;
+            font-weight: bold;
+        }
+        .listProfile {
+            width: 50px;
+            height: 40px;
+            padding: 5px;
+            text-align:center;
+            color: #333333;
+            background-color: white;
+            border-color: gray;
+            border-style:solid;
+            border-width: 2px;
+            font-variant:small-caps;
+            font-weight:bold;
+        }
+        .tagsProfile {
+            height: 40px;
+            padding: 5px;
+            text-align:center;
+            color: #333333;
+            background-color: white;
+            border-color: gray;
+            border-style:solid;
+            border-width: 2px;
+            font-variant:small-caps;
+            font-weight:bold;
+        }
+    </style>
+
+    
+</asp:Content>

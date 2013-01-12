69e069879bc54d5d6f73f1f01dc490e761c7f189
Tiago <Tiago@ASUSteelwolf.dei.isep.ipp.pt>
2013-01-12 12:23:03 +0000

    Criar um grafo nivel 3 para o WebService no IA

---
+++ IA/Web/App_Code/IModuloIA.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ImoduloIA" in both code and config file together.
+[ServiceContract]
+public interface IModuloIa
+{
+	[OperationContract]
+	int tamanhoRede(int nivel,string user);
+
+    [OperationContract]
+    string amigosTag(string user, string tags);
+
+    [OperationContract]
+    string sugerirAmigos(string user);
+
+    [OperationContract]
+    string maven(string tag);
+
+    [OperationContract]
+    string amigosComuns(string user1, string user2 );
+
+    [OperationContract]
+    string caminhoForte(string userOrigem, string userDestino);
+
+    [OperationContract]
+    string caminhoCurto(string userOrigem, string userDestino);
+
+    [OperationContract]
+    float grauMedioSeparacao(string userOrigem, string userDestino);
+
+    [OperationContract]
+    string debug();
+
+}

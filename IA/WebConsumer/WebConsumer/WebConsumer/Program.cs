b133e7c8d4404b6849877f5c8ffb5dcd1a3a26ea
Administrador <Administrador@Bruno_Portatil.dei.isep.ipp.pt>
2013-01-14 09:53:34 +0000

    Bruno: .Exe para Teste autom?tico webservice prolog

--- IA/WebConsumer/WebConsumer/WebConsumer/Program.cs
+++ IA/WebConsumer/WebConsumer/WebConsumer/Program.cs
@@ -26,35 +26,43 @@ namespace WebConsumer
 
           p.WaitForExit();*/
 
-          Console.Write("A Pedir tamanho de rede, aguarde...\n");
+          Console.Write("A Pedir tamanho de rede, aguarde (deve ser 1)...\n");
           string r = proxy.tamanhoRede(2, "sara").ToString();
           Console.WriteLine(r);
 
-          Console.WriteLine("A pedir rede nivel 3, aguarde...");
+          Console.WriteLine("A pedir rede nivel 3, aguarde ([catia,bruno,tiago,hugo])...");
           r = proxy.redeNivel(3, "sara");
           Console.WriteLine(r);
 
-          Console.WriteLine("A pedir amigos tag, aguarde...(deve ser bruno)");
+          Console.WriteLine("A pedir amigos tag, aguarde...(deve ser catia)");
           r = proxy.amigosTag("sara", "chelsea");
           Console.WriteLine(r);
           
-          Console.WriteLine("A pedir sugestoes amigos, aguarde (deve ser vazio)...");
+          Console.WriteLine("A pedir sugestoes amigos carlos, aguarde (deve ser vazio)...");
           r = proxy.sugerirAmigos("carlos");
+          Console.WriteLine(r);
+
+          Console.WriteLine("A pedir sugestoes amigos sara, aguarde (Bruno)...");
+          r = proxy.sugerirAmigos("sara");
           Console.WriteLine(r);
 
           Console.WriteLine("A pedir maven, aguarde (mario)...");
           r = proxy.maven("comida");
           Console.WriteLine(r);
 
-          Console.WriteLine("A pedir amigosComuns, aguarde (? mario,)...");
+          Console.WriteLine("A pedir maven, aguarde (nao existente)...");
+          r = proxy.maven("chelsea");
+          Console.WriteLine(r);
+
+          Console.WriteLine("A pedir amigosComuns, aguarde (mario,)...");
           r = proxy.amigosComuns("bruno","carlos");
           Console.WriteLine(r);
 
-          Console.WriteLine("A pedir caminhoForte hugo-sara, aguarde(hugo,mario,bruno,catia,sara)...");
+          Console.WriteLine("A pedir caminhoForte hugo-sara, aguarde (hugo,mario,bruno,catia,sara)...");
           r = proxy.caminhoForte("hugo", "sara");
           Console.WriteLine(r);
 
-          Console.WriteLine("A pedir caminhoCurto hugo-sara, aguarde(hugo,catia,sara)...");
+          Console.WriteLine("A pedir caminhoCurto hugo-sara, aguarde (hugo,catia,sara)...");
           r = proxy.caminhoCurto("hugo", "sara");
           Console.WriteLine(r);
 
@@ -62,7 +70,11 @@ namespace WebConsumer
           float f = proxy.grauMedioSeparacao();
           Console.WriteLine(f);
 
-          Console.WriteLine("termine o programa.");
+          Console.WriteLine("A pedir grafoNivel3 carlos, aguarde([carlos,mario,2],[mario,carlos,1],...)");
+          r = proxy.grafoNivel3("carlos");
+          Console.WriteLine(r);
+
+          Console.WriteLine("\ntermine o programa...");
           Console.ReadLine();
       }
     }

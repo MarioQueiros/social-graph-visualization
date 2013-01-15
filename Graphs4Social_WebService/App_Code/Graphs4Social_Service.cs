aa1b1f5056ed5748d421428dae70e20e71ed8c19
MpApQ <MpApQ@localhost>
2013-01-15 16:14:16 +0000

    Sugest?es de amigos, pesquisa por tags, mavens, tamanhos de rede, grau m?dio de separa??o e outros bugs

---
+++ Graphs4Social_WebService/App_Code/Graphs4Social_Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+public class Graphs4Social_Service : IGraphs4Social_Service
+{
+
+
+    public string carregaGrafo(string username)
+    {
+        Grafo grafo = new Grafo(username,"");
+        return grafo.toString();
+    }
+
+    public string carregaGrafoAmigosComum(string username1, string username2)
+    {
+        Grafo grafo = new Grafo(username1, username2);
+        return grafo.toString();
+    }
+}
